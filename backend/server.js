52cbbf20af95eefdabaee4c889127a95c4208353
Gaurav <varsolkargaurav@gmail.com>
2026-05-12 12:14:51 +0530

    okkk

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express');
+const { expressMiddleware } = require('@apollo/server/express4');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
